899414845b673853b559a5719f7fd62614c8a7e5
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2017-11-01 07:03:40 +0700

    Update and rename server.sh to lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -6,4 +6,4 @@ sudo add-apt-repository ppa:nijel/phpmyadmin &&
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db &&
 sudo add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/10.0/ubuntu trusty main' &&
 sudo apt-get update &&
-sudo apt-get install mariadb-server php7.0 php7.0-mcrypt php7.0-mbstring php7.0-gd php7.0-zip php7.0-mysql apache2
+sudo apt-get install mariadb-server php7.0 php7.0-mcrypt php7.0-mbstring php7.0-gd php7.0-zip php7.0-mysql apache2 phpmyadmin
