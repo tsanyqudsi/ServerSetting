fdff81c827c173959b2a25e5049c5dccda89312c
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2017-11-01 07:02:47 +0700

    Update and rename server to server.sh

--- server.sh
+++ server.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db &&
 sudo apt-get install software-properties-common &&
+sudo add-apt-repository ppa:nijel/phpmyadmin &&
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db &&
 sudo add-apt-repository 'deb http://ftp.osuosl.org/pub/mariadb/repo/10.0/ubuntu trusty main' &&
 sudo apt-get update &&
-sudo apt-get install mariadb-server
+sudo apt-get install mariadb-server php7.0 php7.0-mcrypt php7.0-mbstring php7.0-gd php7.0-zip php7.0-mysql apache2
